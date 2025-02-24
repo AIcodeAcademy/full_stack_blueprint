75f5e2dcec28f12da8f02a4eeac459a5dda8066a
Alberto Basalo <albertobasalo@hotmail.com>
2025-02-24 13:18:32 +0100

    clean app

--- src/client/shared/navigation.utils.ts
+++ src/client/shared/navigation.utils.ts
@@ -1,12 +1,10 @@
 import { AboutPage } from "../app/about/about.page";
-import { AddAssetPage } from "../app/add-asset/add-asset.page";
 import { AuthPage } from "../app/auth/auth.page";
 import { HomePage } from "../app/home/home.page";
 
 customElements.define("app-about-page", AboutPage);
 customElements.define("app-home-page", HomePage);
 customElements.define("app-auth-page", AuthPage);
-customElements.define("app-add-asset-page", AddAssetPage);
 
 export const navigate = (path: string | null) => {
 	const pageComponent = path
